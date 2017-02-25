4ea6b913e5040dd4f6e841f84f648df45cc34d8b
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2017-02-25 16:34:41 +0100

    Go back to hunting after shooting instead of aiming again.

--- deploy/SharpShooter-Deploy/aiProject/StateController.cs
+++ deploy/SharpShooter-Deploy/aiProject/StateController.cs
@@ -66,7 +66,7 @@ namespace Turing
         {
             if(vector.DamageProb >= 0.8f && vector.ShootDelay == 0)
             {
-                return new ShootDamnItState(currentState);
+                return new ShootDamnItState(new PrepState(new HuntState()));
             }
 
             return null;
